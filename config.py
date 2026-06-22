30a0624a20ab4add79084ce4c04b18a654f93793
Rahul P R <88572526+mrahulpr@users.noreply.github.com>
2026-06-22 15:17:42 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import telebot
+
+BOT_TOKEN = "8532515086:AAEv3PznfTARrV6VFZISRExKOfFYdQcOGMI"
+OWNER_ID = 5705025334
+
+# Initialize the bot here to prevent import loops
+bot = telebot.TeleBot(BOT_TOKEN)
