ba32d9541a286a71f771596cd312566da1a6d5c8
Rahul P R <88572526+mrahulpr@users.noreply.github.com>
2026-06-22 15:22:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ from config import bot
 
 def load_plugins():
     # Scan the plugins folder and import every python file
-    for file in os.listdir("plugins"):
+    for file in os.listdir("start"):
         if file.endswith(".py") and not file.startswith("__"):
-            importlib.import_module(f"plugins.{file[:-3]}")
+            importlib.import_module(f"start.{file[:-3]}")
 
 if __name__ == "__main__":
     load_plugins()
